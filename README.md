85ec7d8be31c8a2c580419106c6a4c571ba20f21
arllan <arllanpablo@gmail.com>
2018-11-16 15:28:36 -0300

    adicionado primeiros exemplos

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Arquivos base para iniciar projetos em bootstrap versão 3.3
 # links
 * (guia do bootstrap de como iniciar projetos)  https://getbootstrap.com/docs/3.3/getting-started/
+
+* (EXPLICAÇÃO DOS METODO AJAX) http://blog.danielbonifacio.com.br/2018/02/ajax-em-5-minutos.html
---
+++ js/ajax.js
@@ -0,0 +1,15 @@
+// aprendendo ajax
+
+/* o metodo ajax no Jquery e chamado da seguinte formas "$.ajax()" dentro do método e recebido um objeto 
+
+$.ajax({
+ url: 'meuscript.php', // destino da requisição
+ method: 'post', // metodo da requisição POST ou GET... 
+ data: { // o data e os parametros que vão ser enviados pela requisição
+  nome: 'Daniel', // parametro a ser enviado. Fica na seguinte ordem nome e informação 
+  idade: 19, // parametro a ser enviado. Fica na seguinte ordem nome e informação 
+  endereco: 'Rua Lobato, Cobilandia' // parametro a ser enviado. Fica na seguinte ordem nome e informação 
+ } // fim so data
+}); // fim do objeto
+
+*/
